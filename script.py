2bc39bd9775ce858ec58bfd24d0e7f5f7a4e8ee3
demevale <cesar.civilmatica@gmail.com>
2024-07-04 06:08:05 +0100

    Initial commmit

---
+++ script.py
@@ -0,0 +1,8 @@
+'''
+   Create a simple Python script called script.py that
+prints "Hello World". Track this file and commit it to
+the repository with the message "Initial commit".
+'''
+
+print("Hello World")
+print("Initial commit")
