18408dc7f81094d39a46e39fc2da96f5ff9c39b3
demevale <cesar.civilmatica@gmail.com>
2024-07-07 01:39:59 +0100

    Initial commit

---
+++ name_input.py
@@ -0,0 +1,11 @@
+'''
+Using your repository on your local machine, create a simple Python script
+that takes your input and prints it to your screen,
+save this as “name_input” and push it to your repository on Github
+'''
+
+# type your name
+name = input("Type your name: ")
+
+# print the name
+print(f"Hi {name}. \nThank you for writing your name.")
