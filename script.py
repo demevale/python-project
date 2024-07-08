0b2b507ffa7b3ecca16fc2e4861ccf75147eb099
demevale <cesar.civilmatica@gmail.com>
2024-07-08 22:11:30 +0100

    Initial commit for creating the first Tag

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 '''
-   Create a simple Python script called script.py that
-prints "Hello World". Track this file and commit it to
-the repository with the message "Initial commit".
+   Make changes to script.py to print your name.
+   Commit this change with the message "Print name"
+   and create a tag for this commit
 '''
 
 print("Hello World")
+myName = "Cesar"
+print(f" My name is {myName}")
