3a826264cefb2bcc6d8a9a6731448dd3dea6953b
demevale <cesar.civilmatica@gmail.com>
2024-07-10 12:29:24 +0100

    script.py

--- script.py
+++ script.py
@@ -1,9 +1,17 @@
 '''
    Make changes to script.py to print your name.
-   Commit this change with the message "Print name"
-   and create a tag for this commit
+   Commit this change with the message "Print name",
+   create a tag for this commit
+   and Make changes to print the current date
 '''
+
+import datetime as dt
 
 print("Hello World")
 myName = "Cesar"
 print(f" My name is {myName}")
+
+
+#Make changes to print the current date
+currentDate = dt.date.today()
+print(f" Current date: {currentDate}")
